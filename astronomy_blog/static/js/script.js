839916f6200151c9ebdc95a5361f6c3c1929f9dd
Sameer Patil <101512997+SamPat8876@users.noreply.github.com>
2025-07-21 10:10:23 +0530

    Update

--- astronomy_blog/static/js/script.js
+++ astronomy_blog/static/js/script.js
@@ -16,3 +16,22 @@ function closeNav() {
 // Attach events
 burger.addEventListener('click', openNav);
 closeBtn.addEventListener('click', closeNav);
+
+// Loader
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loadergif");
+    const main = document.getElementById("main-content");
+
+    if (loader && main) {
+      loader.classList.add("fade-out");
+
+      setTimeout(() => {
+        loader.style.display = "none";
+        main.style.display = "block";
+      }, 500);  // Time should match CSS fade transition
+    }
+});
+
+setTimeout(() => {
+  document.getElementById("main-content").style.display = "block";
+}, 3000);  // fallback if 'load' doesn't fire
